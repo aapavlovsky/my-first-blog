cd065ceed797bed839e404f64295172af2ac28e3
aapavlovsky <aapavlovskii@yandex.ru>
2021-04-09 11:25:04 +0300

    changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2y(8433!)hgq5p^!$ew@cu6ej!de!ue0ea06$4$qkg_f&qcp^n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'aapavlovsky.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
